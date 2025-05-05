27bc5f543379541e426c4d42350a6bddab5e233c
Michael Scott Weber <mscottweber@gmail.com>
2025-05-05 16:56:36 -0500

    Updates build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    // library entry and output settings
+    lib: {
+      entry: resolve(__dirname, 'lib/main.ts'),
+      name: 'possum-ui',
+      fileName: 'possum-ui',
+    },
+    // bundler options
+    // externalize react-related imports
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'react/jsx-runtime': 'react/jsx-runtime',
+        },
+      },
+    },
+  },
+});
